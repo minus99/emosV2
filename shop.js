a3317f8b0db9b8ab02b122c0ed359cacdefa573c
burakkarakaya <burakkarakaya1984@gmail.com>
2017-03-24 16:50:26 +0200

    cookie ve dragula yapısı değişti

--- shop.js
+++ shop.js
@@ -1,9 +1,102 @@
+/*jslint browser: true */ /*global jQuery: true */
+
+/**
+ * jQuery Cookie plugin
+ *
+ * Copyright (c) 2010 Klaus Hartl (stilbuero.de)
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ */
+
+// TODO JsDoc
+
+/**
+ * Create a cookie with the given key and value and other optional parameters.
+ *
+ * @example $.cookie('the_cookie', 'the_value');
+ * @desc Set the value of a cookie.
+ * @example $.cookie('the_cookie', 'the_value', { expires: 7, path: '/', domain: 'jquery.com', secure: true });
+ * @desc Create a cookie with all available options.
+ * @example $.cookie('the_cookie', 'the_value');
+ * @desc Create a session cookie.
+ * @example $.cookie('the_cookie', null);
+ * @desc Delete a cookie by passing null as value. Keep in mind that you have to use the same path and domain
+ *       used when the cookie was set.
+ *
+ * @param String key The key of the cookie.
+ * @param String value The value of the cookie.
+ * @param Object options An object literal containing key/value pairs to provide optional cookie attributes.
+ * @option Number|Date expires Either an integer specifying the expiration date from now on in days or a Date object.
+ *                             If a negative value is specified (e.g. a date in the past), the cookie will be deleted.
+ *                             If set to null or omitted, the cookie will be a session cookie and will not be retained
+ *                             when the the browser exits.
+ * @option String path The value of the path atribute of the cookie (default: path of page that created the cookie).
+ * @option String domain The value of the domain attribute of the cookie (default: domain of page that created the cookie).
+ * @option Boolean secure If true, the secure attribute of the cookie will be set and the cookie transmission will
+ *                        require a secure protocol (like HTTPS).
+ * @type undefined
+ *
+ * @name $.cookie
+ * @cat Plugins/Cookie
+ * @author Klaus Hartl/klaus.hartl@stilbuero.de
+ */
+
+/**
+ * Get the value of a cookie with the given key.
+ *
+ * @example $.cookie('the_cookie');
+ * @desc Get the value of a cookie.
+ *
+ * @param String key The key of the cookie.
+ * @return The value of the cookie.
+ * @type String
+ *
+ * @name $.cookie
+ * @cat Plugins/Cookie
+ * @author Klaus Hartl/klaus.hartl@stilbuero.de
+ */
+jQuery.cookie = function (key, value, options) {
+    
+    // key and at least value given, set cookie...
+    if (arguments.length > 1 && String(value) !== "[object Object]") {
+        options = jQuery.extend({}, options);
+
+        if (value === null || value === undefined) {
+            options.expires = -1;
+        }
+
+        if (typeof options.expires === 'number') {
+            var days = options.expires, t = options.expires = new Date();
+            t.setDate(t.getDate() + days);
+        }
+        
+        value = String(value);
+        
+        return (document.cookie = [
+            encodeURIComponent(key), '=',
+            options.raw ? value : encodeURIComponent(value),
+            options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+            options.path ? '; path=' + options.path : '',
+            options.domain ? '; domain=' + options.domain : '',
+            options.secure ? '; secure' : ''
+        ].join(''));
+    }
+
+    // key and possibly options given, get cookie...
+    options = value || {};
+    var result, decode = options.raw ? function (s) { return s; } : decodeURIComponent;
+    return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? decode(result[1]) : null;
+};
+
 
 // shop admin
 
 var table;
 
 var Admin = {
+	win: $( window ),
     definitions:{
         mainTabs: null,
         currentID: null,
@@ -28,25 +121,43 @@ var Admin = {
                 }
             });
         },
+		cookies: function( o ){ 
+			var typ = o['typ'] || '', name = o['name'] || '';
+			if( typ == 'set' ){ 
+				var date = new Date(), minutes = o['minutes'] || 60;
+					date.setTime( date.getTime() + ( minutes * 60 * 1000 ) );
+				$.cookie(name, o['value'] || '', { expires: date, path: '/' });
+			}else if( typ == 'get' )
+				return $.cookie( name );
+		},
 		getTabID: function( ID ){
 			var _t = Admin;
 			ID = $( ID );
 			if( ID.hasClass('ui-tabs-panel') ) id = ID.attr('id') || '';
 			else id = ID.parents('.ui-tabs-panel').eq( 0 ).attr('id') || '';
 			return '#' + id;
+		},
+		getTabObj: function(){
+			var obj = {};
+			$('#main-tabs .nav-tabs li')
+			.each(function( i ){
+				var ths = $( this ), ID = ths.find('a'), k = ths.attr('aria-controls') || '';
+				obj[ k ] = { 'open': i, 'url': ID.attr('data-url') || '', 'ID': k, 'title': ID.attr('title') || '' };
+			});
+			return obj;
 		}
-    },
+    }, 
     init: function(){
         var t = this,
             //currentID = null,
-            tabsCookies = t.cookies.check("@alaashopcookies");
-            
+            tabsCookies = t.uty.cookies({ typ: 'get', name: 'tabCookies' });
+        
         //t.definitions.currentID = currentID;
         
         // check for tabs cookies to open them
         if (tabsCookies != null)
         {    
-            var tabs = JSON.parse( tabsCookies );
+            var tabs = JSON.parse( tabsCookies );  
             t.definitions.tabsCookies = tabs;
             
             t.activateCurrentTab();
@@ -102,7 +213,7 @@ var Admin = {
             
             var sorted = makeArrayAndSortBy(tabs, "open");
             
-            console.log(sorted);
+            console.log('openOtherTabsFromCookies', sorted);
             
             //console.log(tabs);
             //var sorted = Object.keys(tabs).sort(function(a,b){return tabs["open"]-tabs["open"]});
@@ -264,19 +375,22 @@ var Admin = {
             });
         },
         refreshCookies: function(){
-            Admin.cookies.write("@alaashopcookies", JSON.stringify(this.openTabs));
+			var obj = Admin.uty.getTabObj();
+			Admin.definitions.tabsCookies = obj;
+			Admin.uty.cookies({ typ: 'set', name: 'tabCookies', value: JSON.stringify( obj ) });
         },
         refreshOrder: function(){
             var t = this;
             $("#main-tabs > ul > .ui-tabs-tab").each(function(index){
                 var id = $("a", this).attr("href").replace("#", "");
-                
+                /*
                 t.openTabs[id].open = index;
                 t.refreshCookies();
                 
                 console.log(id, index);
-                
+                */
             });
+			t.refreshCookies();
         },
         index: 0,
         openTabs: {}
@@ -331,7 +445,9 @@ var Admin = {
             
             dragula([document.querySelector(_ID+" #left-defaults"), document.querySelector(_ID+" #right-defaults")], {
                 moves: function(el, container, handle){ return handle.classList.contains('handle'); }
-            });
+            }).on('drop', function ( el ){
+				Admin.win.resize();
+			});
         },
         // activate data table grid
         table: function( ID ){
@@ -411,21 +527,6 @@ var Admin = {
         if (history && history.pushState){
             var obj = {Page:page, Url:url};
             history.pushState(obj, obj.Page, obj.Url);
-        }
-    },
-    
-    // cookies manager
-    cookies: {
-        write: function(key, value, days){
-            var _days = days || 7;
-            var d = new Date();
-                d.setTime(d.getTime() + (_days*24*60*60*1000));
-            document.cookie = key + "=" + value + ";   expired=" + d.toGMTString();
-        },
-        check: function(key){
-            var value = "; " + document.cookie,
-                parts = value.split("; " + key + "=");
-            return parts.length == 2 ? parts.pop().split(";").shift() : null;
         }
     },
     page:{
